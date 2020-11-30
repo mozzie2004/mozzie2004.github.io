f43b1d742900f227994c8d186ce2e5d26008bc11
Malofiy Yaroslav <70477606+mozzie2004@users.noreply.github.com>
2020-11-30 17:44:21 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,84 @@
+window.addEventListener('DOMContentLoaded', ()=>{
+    'use strict';
+
+    const hamburger = document.querySelector('.hamburger'),
+    close = document.querySelector('.menu__close'),
+    menu = document.querySelector('.menu'),
+    overley = document.querySelector('.menu__overlay');
+
+    hamburger.addEventListener('click', ()=>{
+        menu.classList.add('active');
+    });
+
+    close.addEventListener('click', ()=>{
+        menu.classList.remove('active');
+    });
+
+    overley.addEventListener('click', ()=>{
+        menu.classList.remove('active');
+    });
+
+    // form 
+
+    async function postForm(url='', data={}){
+        const response = await fetch(url, {
+            method: 'POST',
+            body: data
+        });
+
+        return await response.text();
+    }
+
+    // form 
+
+    async function postForm(url='', data={}){
+        const response = await fetch(url, {
+            method: 'POST',
+            body: data
+        });
+    
+        return await response.text();
+    }
+
+    const form = document.querySelector('form'),
+        spinner = document.querySelector('.lds-ellipsis'),
+        content = document.querySelector('.contacts__content');
+
+    form.addEventListener('submit', (e)=>{
+        e.preventDefault();
+        spinner.classList.remove('lds-ellipsis_hidden');
+        const formData = new FormData(form);
+        postForm('php/server.php', formData)
+        .then(res=>{
+            spinner.classList.add('lds-ellipsis_hidden');
+            const div = document.createElement('div');
+            if(res==='success') {
+               div.classList.add('message_ok');
+               content.append(div);
+               div.textContent = 'Ваше повідомлення відправлене';
+               setTimeout(()=>{
+                    div.remove();
+               }, 3000);
+               
+            }else {
+                div.classList.add('message_er');
+                content.append(div);
+                div.textContent = 'Спробуйте пізніше';
+               setTimeout(()=>{
+                    div.remove();
+               }, 3000);
+            }
+        })
+        .catch(err=>{
+            spinner.classList.add('lds-ellipsis_hidden');
+            const divEr = document.createElement('div');
+            divEr.classList.add('message_er');
+                content.append(divEr);
+                divEr.textContent = 'Спробуйте пізніше1';
+               setTimeout(()=>{
+                    divEr.remove();
+               }, 3000);
+        })
+    })
+})
+
